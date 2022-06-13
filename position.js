11794a84728392ff4b3100bcf772978f65e9cb7e
max k <maxkuhn@gmail.com>
2022-06-12 21:48:29 -0700

    Added position finder.

---
+++ position.js
@@ -0,0 +1,18 @@
+function finalPosition(moves) {
+  let position = [0,0]
+  for (const move of moves) {
+    if (move === "north") {
+      position[1]++;
+    } else if (move === "west") {
+      position[0]--;
+    } else if (move === "south") {
+      position[1]--;
+    } else if (move === "east") {
+      position[0]++;
+    }
+  }
+  return position;
+}
+
+const directions = ['north', 'north', 'west', 'west', 'north', 'east','north'];
+console.log(finalPosition(directions));
