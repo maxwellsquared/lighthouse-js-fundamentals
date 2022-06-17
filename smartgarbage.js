4952bb9482ee31b0ef9b3591c555cbad48c11126
max k <maxkuhn@gmail.com>
2022-06-17 10:11:43 -0700

    added learning files

---
+++ smartgarbage.js
@@ -0,0 +1,10 @@
+function smartGarbage(trash, bins) {
+  if (trash === 'recycling') {
+    bins.recycling += 1;
+  } else if (trash === 'waste') {
+    bins.waste += 1;
+  } else if (trash === 'compost') {
+    bins.compost += 1;
+  } 
+  return bins;
+}
